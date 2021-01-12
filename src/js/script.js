504895f25f62cb92eeb611e7c2527031cfc1664f
Artem <klaid-87@mail.ru>
2021-01-12 22:00:49 +0300

    Закончил стилизацию блока banner-section. Подключил slick-slider.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 $(function() {
-  $('.banner-section__slider').slick({});
+  $('.banner-section__slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-button banner-section__slider-button--prev button" type="button"><img src="./images/svg/arrow-prew.svg" alt="Стрелка переключения влево"></button>',
+    nextArrow: '<button class="banner-section__slider-button banner-section__slider-button--next button" type="button"><img src="./images/svg/arrow-next.svg" alt="Стрелка переключения вправо"></button>'
+  });
 });

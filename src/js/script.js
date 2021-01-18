085294767bbed44a4d38f56e3a6bb4efdb57763f
Artem <klaid-87@mail.ru>
2021-01-18 20:07:52 +0300

    Добавил универсальный класс для табов

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,13 @@ $(function() {
     prevArrow: '<button class="banner-section__slider-button banner-section__slider-button--prev button" type="button"><img src="./images/svg/arrow-prew.svg" alt="Стрелка переключения влево"></button>',
     nextArrow: '<button class="banner-section__slider-button banner-section__slider-button--next button" type="button"><img src="./images/svg/arrow-next.svg" alt="Стрелка переключения вправо"></button>'
   });
+
+  $('.tab').on('click', function(e) {
+    e.preventDefault();
+    $('.tab').removeClass('tab--active');
+    $('.tabs__content').removeClass('tabs__content--active');
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('tabs__content--active');
+  });
 });
+

cc67d05630fe10b8ba703450ce5d7905e9c36603
Artem <klaid-87@mail.ru>
2021-01-30 12:07:49 +0300

    Верстка главной страницы

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 $(function() {
   $('.banner-section__slider').slick({
     dots: true,
+    dotsClass: 'banner-section__dots',
     prevArrow: '<button class="banner-section__slider-button banner-section__slider-button--prev button" type="button"><img src="./images/svg/arrow-prew.svg" alt="Стрелка переключения влево"></button>',
     nextArrow: '<button class="banner-section__slider-button banner-section__slider-button--next button" type="button"><img src="./images/svg/arrow-next.svg" alt="Стрелка переключения вправо"></button>'
   });
@@ -8,13 +9,22 @@ $(function() {
   $('.tab').on('click', function(e) {
     e.preventDefault();
     $($(this).siblings()).removeClass('tab--active');
-    $($(this).parent().siblings().find('.tabs__content')).removeClass('tabs__content--active');
+    $($(this).parent().siblings().find('.tabs-content')).removeClass('tabs-content--active');
     $(this).addClass('tab--active');
-    $($(this).attr('href')).addClass('tabs__content--active');
+    $($(this).attr('href')).addClass('tabs-content--active');
   });
 
   $('.product-item__favourite').on('click', function() {
-    $('.product-item__favourite').toggleClass('product-item__favourite--active')
+    $(this).toggleClass('product-item__favourite--active')
   });
+
+  $('.products__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button class="products__slider-button products__slider-button--prev button" type="button"><img src="./images/svg/arrow-prew-black.svg" alt="Стрелка переключения влево"></button>',
+    nextArrow: '<button class="products__slider-button products__slider-button--next button" type="button"><img src="./images/svg/arrow-next-black.svg" alt="Стрелка переключения вправо"></button>'
+  });
+
+
 });
 

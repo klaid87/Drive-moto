82b9fea9f9f08ee117700746b4d7766815bb8d02
Artem <klaid-87@mail.ru>
2021-02-02 21:15:38 +0300

    Приступил к стилизации страницы каталога

--- src/js/script.js
+++ src/js/script.js
@@ -21,6 +21,7 @@ $(function() {
   $('.products__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
+    variableWidth: true,
     prevArrow: '<button class="products__slider-button products__slider-button--prev button" type="button"><img src="./images/svg/arrow-prew-black.svg" alt="Стрелка переключения влево"></button>',
     nextArrow: '<button class="products__slider-button products__slider-button--next button" type="button"><img src="./images/svg/arrow-next-black.svg" alt="Стрелка переключения вправо"></button>'
   });

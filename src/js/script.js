6fe3e868ad343e598b23bb9f7309b5b5e960616f
Artem <klaid-87@mail.ru>
2021-02-17 22:46:06 +0300

    Закончил адаптив блока search

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,21 @@ $(function() {
     dots: true,
     dotsClass: 'banner-section__dots',
     prevArrow: '<button class="banner-section__slider-button banner-section__slider-button--prev button" type="button"><img src="./images/svg/arrow-prew.svg" alt="Стрелка переключения влево"></button>',
-    nextArrow: '<button class="banner-section__slider-button banner-section__slider-button--next button" type="button"><img src="./images/svg/arrow-next.svg" alt="Стрелка переключения вправо"></button>'
+    nextArrow: '<button class="banner-section__slider-button banner-section__slider-button--next button" type="button"><img src="./images/svg/arrow-next.svg" alt="Стрелка переключения вправо"></button>',
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          adaptiveHeight: true
+        }
+      },
+      {
+        breakpoint: 960,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
   });
 
   $('.tab').on('click', function(e) {

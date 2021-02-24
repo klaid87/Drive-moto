b2450b797b37d2d8c5c1b5a29e931ef56c998f89
Artem <klaid-87@mail.ru>
2021-02-24 21:01:02 +0300

    Закончил адаптив страницы catalog. Приступил к адаптиву страницы product

--- src/js/script.js
+++ src/js/script.js
@@ -35,7 +35,7 @@ $(function() {
   $('.products__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    variableWidth: false,
+    variableWidth: true,
     prevArrow: '<button class="products__slider-button products__slider-button--prev button" type="button"><img src="./images/svg/arrow-prew-black.svg" alt="Стрелка переключения влево"></button>',
     nextArrow: '<button class="products__slider-button products__slider-button--next button" type="button"><img src="./images/svg/arrow-next-black.svg" alt="Стрелка переключения вправо"></button>',
     responsive: [
@@ -116,6 +116,10 @@ $(function() {
   $('.page-footer__title').on('click', function() {
     $(this).next().slideToggle();
     $(this).toggleClass('page-footer__title--active')
+  });
+
+  $('.catalog__button-filter').on('click', function() {
+    $(this).next().slideToggle();
   });
 
 });
